f8c777897a047185e70313e2855352f5100c8529
Drew Bollinger <drew.bollinger@gsa.gov>
2024-03-12 16:01:03 -0700

    stable chrome, amd64

--- tasks/a11y/build.sh
+++ tasks/a11y/build.sh
@@ -8,9 +8,9 @@ apt install npm --no-install-recommends -y
 npm install @axe-core/cli -g
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-  && echo 'deb [arch=arm64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google.list \
+  && echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google.list \
   && apt-get update \
-  && apt-get install -y google-chrome-unstable --no-install-recommends \
+  && apt-get install -y google-chrome-stable --no-install-recommends \
   && rm -rf /var/lib/apt/lists/*
 
 python -c "import chromedriver_autoinstaller;chromedriver_autoinstaller.install()"
