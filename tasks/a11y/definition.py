f816d67fbb92384b7b1ef4a9f2d8be0047ccd8c8
Drew Bollinger <drew.bollinger@gsa.gov>
2024-03-26 15:37:20 -0700

    bundle final report

--- tasks/a11y/definition.py
+++ tasks/a11y/definition.py
@@ -1,6 +1,11 @@
-from lib.task import BaseBuildTask
+import datetime
+import os
+import shutil
 import subprocess
+
 from scraper.spider import process, A11ySpider
+
+from lib.task import BaseBuildTask
 
 
 class BuildTask(BaseBuildTask):
@@ -11,31 +16,42 @@ class BuildTask(BaseBuildTask):
 
     def handler(self):
         """scan"""
-        filename = 'report.html'
+        target = self.args['target']
+        results_dir = 'results'
+        reports_dir = 'reports'
+        os.makedirs(results_dir, exist_ok=True)
 
+        # crawl
         data = []  # accumulates the urls during .start()
-        process.crawl(A11ySpider, target=self.args['target'], data=data)
+        process.crawl(A11ySpider, target=target, data=data)
         process.start()
 
+        # scan
         for url in data:
             subprocess.run([
                 'axe',
-                '--chrome-options="no-sandbox,disable-setuid-sandbox,disable-dev-shm-usage"',
+                '--chrome-options="no-sandbox,disable-setuid-sandbox,disable-dev-shm-usage"',  # noqa: E501
                 url,
                 '--tags',
                 'wcag2a,wcag2aa,wcag21a,wcag21aa,wcag22aa',
                 '--dir',
-                'results'
+                results_dir
             ], timeout=900)
 
+        # report
         subprocess.run([
             'node',
             'reporter/generate-reporter.js',
             '--inputDir',
-            'results',
+            results_dir,
             '--outputDir',
-            'reports',
+            reports_dir,
             '--target',
-            self.args['target']
+            target
         ])
-        return f'{filename}'
+
+        # bundle
+        today = f'{datetime.date.today():%Y-%m-%d-%M}'
+        filename = f'accessibility-scan-for-{target}-on-{today}.zip'
+        shutil.make_archive(filename, 'zip', reports_dir)
+        return filename
