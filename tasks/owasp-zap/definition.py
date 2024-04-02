53e9126d5613de3a0757169faa1a287cf099fc21
Sarah Rudder <sarah.rudder@gsa.gov>
2024-04-02 12:50:00 -0500

    add report for vulnerability scan to match a11y scan

--- tasks/owasp-zap/definition.py
+++ tasks/owasp-zap/definition.py
@@ -10,11 +10,25 @@ class BuildTask(BaseBuildTask):
 
     def handler(self):
         """scan"""
-        filename = 'report.html'
+        report = 'report.html'
         subprocess.run([
             'zap-baseline.py',
             '-t', self.args['target'],
-            '-r', filename
+            '-r', report
         ], timeout=900)
 
+        subprocess.run([
+            'node',
+            'build-task/reporter/generate-report.js',
+            '--input',
+            report,
+            '--output',
+            filename,
+            '--target',
+            target
+            
+        ])
+
+        # bundle
+        filename = f'/vulnerability-scan-for-{owner}-{repository}-{buildid}'  # noqa: E501
         return f'/zap/wrk/{filename}'
